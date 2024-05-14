aefd2a6ae739cace1b5c3730763455a3eb5b2533
att124 <daniel.fernandez@davinci.edu.ar>
2024-05-14 12:24:13 -0300

    Clase Comprador
    agregada la clase CompradorControlador y CompradorRepository (Interfaz).

--- TrabajoInmobiliaria/src/trabajoInmobiliaria/Garante.java
+++ TrabajoInmobiliaria/src/trabajoInmobiliaria/Garante.java
@@ -1,47 +1,34 @@
 package trabajoInmobiliaria;
 
-public class Garante {
+import java.time.LocalDate;
+
+public class Garante extends Cliente{
 	
 	private int id_garante;
-	private String nombre;
-	private String apellido;
-	private int telefono;
-	public Garante(int id_garante, String nombre, String apellido, int telefono) {
-		super();
+
+	public Garante(String nombre,int id_cliente, String apellido, String correo, int telefono, LocalDate fecha_nac, int dni,int id_garante) {
+		super(nombre,id_cliente,apellido,correo,telefono,fecha_nac,dni);
 		this.id_garante = id_garante;
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.telefono = telefono;
+		
 	}
+	
+	
+	
+
+
 	@Override
 	public String toString() {
-		return "Garante [id_garante=" + id_garante + ", nombre=" + nombre + ", apellido=" + apellido + ", telefono="
-				+ telefono + "]";
+		return "Garante [id_garante=" + id_garante + "]";
 	}
+
+
 	public int getId_garante() {
 		return id_garante;
 	}
 	public void setId_garante(int id_garante) {
 		this.id_garante = id_garante;
 	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getApellido() {
-		return apellido;
-	}
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-	public int getTelefono() {
-		return telefono;
-	}
-	public void setTelefono(int telefono) {
-		this.telefono = telefono;
-	}
+
 
 	
 }
