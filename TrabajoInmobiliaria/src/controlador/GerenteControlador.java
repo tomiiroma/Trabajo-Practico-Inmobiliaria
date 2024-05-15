f994130daa67f6b66ea849a25b6be3e828fb2efb
crumbytom <tomas.roma@davinci.edu.ar>
2024-05-15 12:43:47 -0300

    interfaces+

--- TrabajoInmobiliaria/src/controlador/GerenteControlador.java
+++ TrabajoInmobiliaria/src/controlador/GerenteControlador.java
@@ -1,5 +1,125 @@
 package controlador;
 
-public class GerenteControlador {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import interfaces.GerenteControladorRepository;
+import trabajoInmobiliaria.DatabaseConnection;
+import trabajoInmobiliaria.Gerente;
+import trabajoInmobiliaria.Gerente;
+
+public class GerenteControlador implements GerenteControladorRepository{
+
+	
+	
+
+	 private final Connection connection;
+
+	    public GerenteControlador() {
+	        this.connection = DatabaseConnection.getInstance().getConnection();
+	    }
+	    
+	    
+	    @Override
+	    public List<Gerente> getAllGerente() {
+	        List<Gerente> gerente = new ArrayList<>();
+	     //   try {
+	          //  PreparedStatement statement = connection.prepareStatement("SELECT * FROM users ");
+	           // ResultSet resultSet = statement.executeQuery();
+	       
+	         //   while (resultSet.next()) {
+	        // public Gerente(String nombre,int id_cliente, String apellido, String correo, int telefono, LocalDate fecha_nac, int dni,int comprador, double presupuesto)
+	            //	Gerente Gerentes = new Gerente(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getString("email"));
+	             //   users.add(Gerente);
+	      //      }
+	     //   } catch (SQLException e) {
+	     //       e.printStackTrace();
+	   //     }
+	    //    return users;
+	        return gerentes;
+	    }
+
+	    
+	    @Override
+	    public Gerente getGerenteById(int id) {
+	    	Gerente gerente = null;
+	        try {
+	            PreparedStatement statement = connection.prepareStatement("SELECT * FROM users WHERE id = ?");
+	            statement.setInt(1, id);
+	            
+	            ResultSet resultSet = statement.executeQuery();
+	            
+	            if (resultSet.next()) {
+	            	// public Gerente(int comprador, double presupuesto) le falta Herencia con cliente.
+	             //   user = new Garante(resultSet.getInt("id_empleado"), resultSet.getString("name"), resultSet.getString("apellido") , resultSet.getString("telefono"), resultSet.getInt("fk_cliente"));
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	        return gerente;
+	    }
+
+		@Override
+	    public void addGerente(Gerente gerente) {
+	        try {
+	            PreparedStatement statement = connection.prepareStatement("INSERT INTO users (name, correo) VALUES (?, ?)");
+	            statement.setString(1, gerente.getNombre());
+	            statement.setString(2, gerente.getCorreo());
+	            
+	            int rowsInserted = statement.executeUpdate();
+	            if (rowsInserted > 0) {
+	                System.out.println("Gerente insertado exitosamente");
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+		@Override
+	    public void updateGerente(Gerente gerente) {
+	        try {
+	            PreparedStatement statement = connection.prepareStatement("UPDATE users SET name = ?, apellido = ? WHERE id = ?");
+	            statement.setString(1, gerente.getNombre());
+	            statement.setString(2, gerente.getApellido());
+	            statement.setInt(3, gerente.getId_cliente());
+	            
+	            int rowsUpdated = statement.executeUpdate();
+	            if (rowsUpdated > 0) {
+	                System.out.println("Gerente actualizado exitosamente");
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+	    @Override
+	    public void deleteGerente(int id) {
+	        try {
+	            PreparedStatement statement = connection.prepareStatement("DELETE FROM users WHERE id = ?");
+	            statement.setInt(1, id);
+	            
+	            int rowsDeleted = statement.executeUpdate();
+	            if (rowsDeleted > 0) {
+	                System.out.println("Gerente eliminado exitosamente");
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	
+	
+}
+
+
+
+
+
+	
+	
+	
+	
+	
 }
--- TrabajoInmobiliaria/src/interfaces/GerenteControladorRepository.java
+++ TrabajoInmobiliaria/src/interfaces/GerenteControladorRepository.java
@@ -1,5 +1,21 @@
 package interfaces;
 
+import java.util.List;
+
+import trabajoInmobiliaria.Gerente;
+
 public interface GerenteControladorRepository {
+	
+	
+	//prototipos de metodos 
+    List<Gerente> getAllGerente(); // llama a todos los usuarios de la bdd
+    
+    Gerente getGerenteById(int id); //llama solo a uno, por su id
+    
+    void addGerente(Gerente user); //añade usuarios a la bdd
+    
+    void updateGerente(Gerente user); //actualiza los usuarios de la bdd
+    
+    void deleteGerente(int id); //eliminar usuarios de la bdd
 
 }
