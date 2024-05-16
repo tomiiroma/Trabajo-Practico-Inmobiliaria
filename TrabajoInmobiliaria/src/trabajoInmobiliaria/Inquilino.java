4de1861c34f5b520d5954ec2ca0ac37e99c4e0b2
Tomas Roma <tomas.roma@davinci.edu.ar>
2024-05-16 20:50:14 -0300

    agrego nuevo atributo de tiene_mascota en inquilino

--- TrabajoInmobiliaria/src/trabajoInmobiliaria/Inquilino.java
+++ TrabajoInmobiliaria/src/trabajoInmobiliaria/Inquilino.java
@@ -5,12 +5,17 @@ import java.time.LocalDate;
 public class Inquilino extends Cliente{
 	
 	private int id_inquilino;
+	private boolean tiene_mascota;
 	
+
 	public Inquilino(String nombre, int id_cliente, String apellido, String correo, int telefono, LocalDate fecha_nac,
-			int dni, int id_inquilino, Boolean mascota) {
+			int dni, int id_inquilino, boolean tiene_mascota) {
 		super(nombre, id_cliente, apellido, correo, telefono, fecha_nac, dni);
 		this.id_inquilino = id_inquilino;
+		this.tiene_mascota = tiene_mascota;
 	}
+
+
 
 	public int getId_inquilino() {
 		return id_inquilino;
@@ -19,6 +24,20 @@ public class Inquilino extends Cliente{
 	public void setId_inquilino(int id_inquilino) {
 		this.id_inquilino = id_inquilino;
 	}
+
+
+
+	public boolean isTiene_mascota() {
+		return tiene_mascota;
+	}
+
+
+
+	public void setTiene_mascota(boolean tiene_mascota) {
+		this.tiene_mascota = tiene_mascota;
+	}
+	
+	
 
 
 	
