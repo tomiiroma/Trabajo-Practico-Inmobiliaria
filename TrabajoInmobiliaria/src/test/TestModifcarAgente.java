5e1901e1e99428d0781afb9d71ebce2e2979cb22
att124 <daniel.fernandez@davinci.edu.ar>
2024-05-28 20:13:46 -0300

    Modificar agente faltan algunos casos
    Agregado algunos casos del método modificar tanto en código como en la documentación.

--- TrabajoInmobiliaria/src/test/TestModifcarAgente.java
+++ TrabajoInmobiliaria/src/test/TestModifcarAgente.java
@@ -86,20 +86,170 @@ public class TestModifcarAgente {
 	
 	
 	
+		  @DisplayName("Modificar agente con error en el nombre, Espacios vacios.")
+		  @Test	
+		 public void AgregarAgenteF2() {
+			 
+			  Gerente gerente1 = new Gerente();
+			  
+			 LocalDate fecha_nacimiento = null;
+			 
+			 fecha_nacimiento = LocalDate.of(2000,8, 10);
+			  
+			 	
+			 AgenteControlador agente1 = new AgenteControlador();
+			 	
+			 Agente	agente = new Agente();
+			 
+			 agente = agente1.getAgenteById(2);
+			
+			 	boolean flag=false;
+			 	
+			 	if (gerente1.ModificarAgente2(agente, "asda sd", "asdd", fecha_nacimiento, 42323232, 12345678, "Agente@agente.com", "cobra1234", 3030)==true) {
+			 		
+			 		
+			 		flag = false;
+			 		
+			 		
+			 		
+			 	}
+			
+			 assertEquals(false,flag);
+			 	
+		 }
 	
 	
 	
+		  @DisplayName("Modificar agente con error en el apellido, ingresa numeros.")
+		  @Test	
+		 public void AgregarAgenteF3() {
+			 
+			  Gerente gerente1 = new Gerente();
+			  
+			 LocalDate fecha_nacimiento = null;
+			 
+			 fecha_nacimiento = LocalDate.of(2000,8, 10);
+			  
+			 	
+			 AgenteControlador agente1 = new AgenteControlador();
+			 	
+			 Agente	agente = new Agente();
+			 
+			 agente = agente1.getAgenteById(2);
+			
+			 	boolean flag=false;
+			 	
+			 	if (gerente1.ModificarAgente2(agente, "asdasd", "asdd123", fecha_nacimiento, 42323232, 12345678, "Agente@agente.com", "cobra1234", 3030)==true) {
+			 		
+			 		
+			 		flag = false;
+			 		
+			 		
+			 		
+			 	}
+			
+			 assertEquals(false,flag);
+			 	
+		 }
 	
 	
+		  @DisplayName("Modificar agente con error en el apellido, Espacios vacios.")
+		  @Test	
+		 public void AgregarAgenteF4() {
+			 
+			  Gerente gerente1 = new Gerente();
+			  
+			 LocalDate fecha_nacimiento = null;
+			 
+			 fecha_nacimiento = LocalDate.of(2000,8, 10);
+			  
+			 	
+			 AgenteControlador agente1 = new AgenteControlador();
+			 	
+			 Agente	agente = new Agente();
+			 
+			 agente = agente1.getAgenteById(2);
+			
+			 	boolean flag=false;
+			 	
+			 	if (gerente1.ModificarAgente2(agente, "asdasd", "asd dds", fecha_nacimiento, 42323232, 12345678, "Agente@agente.com", "cobra1234", 3030)==true) {
+			 		
+			 		
+			 		flag = false;
+			 		
+			 		
+			 		
+			 	}
+			
+			 assertEquals(false,flag);
+			 	
+		 }
 	
 	
+		  @DisplayName("Modificar agente con error en el DNI, más de 8 digitos.")
+		  @Test	
+		 public void AgregarAgenteF5() {
+			 
+			  Gerente gerente1 = new Gerente();
+			  
+			 LocalDate fecha_nacimiento = null;
+			 
+			 fecha_nacimiento = LocalDate.of(2000,8, 10);
+			  
+			 	
+			 AgenteControlador agente1 = new AgenteControlador();
+			 	
+			 Agente	agente = new Agente();
+			 
+			 agente = agente1.getAgenteById(2);
+			
+			 	boolean flag=false;
+			 	
+			 	if (gerente1.ModificarAgente2(agente, "asdasd", "asddds", fecha_nacimiento, 423232321, 12345678, "Agente@agente.com", "cobra1234", 3030)==true) {
+			 		
+			 		
+			 		flag = false;
+			 		
+			 		
+			 		
+			 	}
+			
+			 assertEquals(false,flag);
+			 	
+		 }
 	
 	
-	
-	
-	
-	
-	
+		  @DisplayName("Modificar agente con error en el DNI, ingresa número negativo")
+		  @Test	
+		 public void AgregarAgenteF6() {
+			 
+			  Gerente gerente1 = new Gerente();
+			  
+			 LocalDate fecha_nacimiento = null;
+			 
+			 fecha_nacimiento = LocalDate.of(2000,8, 10);
+			  
+			 	
+			 AgenteControlador agente1 = new AgenteControlador();
+			 	
+			 Agente	agente = new Agente();
+			 
+			 agente = agente1.getAgenteById(2);
+			
+			 	boolean flag=false;
+			 	
+			 	if (gerente1.ModificarAgente2(agente, "asdasd", "asddds", fecha_nacimiento, -2332321, 12345678, "Agente@agente.com", "cobra1234", 3030)==true) {
+			 		
+			 		
+			 		flag = false;
+			 		
+			 		
+			 		
+			 	}
+			
+			 assertEquals(false,flag);
+			 	
+		 }
 	
 	
 	
