4f5c9393a8e68eb13b9d856a6b871e2c5919a606
Tomas Roma <tomas.roma@davinci.edu.ar>
2024-06-08 22:08:51 -0300

    a

--- TrabajoInmobiliaria/src/vista/MenuInmuebles.java
+++ TrabajoInmobiliaria/src/vista/MenuInmuebles.java
@@ -5,6 +5,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MenuInmuebles extends JFrame {
 
@@ -31,11 +34,35 @@ public class MenuInmuebles extends JFrame {
 	 */
 	public MenuInmuebles() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 869, 691);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		
+		JButton btnVerInmuebles = new JButton("Ver Inmuebles");
+		btnVerInmuebles.setBounds(301, 117, 220, 55);
+		btnVerInmuebles.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		contentPane.setLayout(null);
+		contentPane.add(btnVerInmuebles);
+		
+		JButton btnAgregarInmueble = new JButton("Agregar Inmuebles");
+		btnAgregarInmueble.setBounds(301, 200, 220, 55);
+		contentPane.add(btnAgregarInmueble);
+		
+		JButton btnModificarInmueble = new JButton("Modificar Inmuebles");
+		btnModificarInmueble.setBounds(301, 298, 220, 55);
+		contentPane.add(btnModificarInmueble);
+		
+		JButton btnEliminarInmueble = new JButton("Eliminar Inmuebles");
+		btnEliminarInmueble.setBounds(301, 391, 220, 55);
+		contentPane.add(btnEliminarInmueble);
+		
+		JButton btnVolver = new JButton("Volver");
+		btnVolver.setBounds(301, 486, 220, 55);
+		contentPane.add(btnVolver);
 	}
-
 }
